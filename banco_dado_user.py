be9c07540b654acb2cc71ad5bad0cfa81ea8cb36
Andredayvsson <dayvssongatinho@gmail.com>
2024-04-09 16:28:00 -0300

    creia a função inserir usuario

--- banco_dado_user.py
+++ banco_dado_user.py
@@ -1,2 +1,15 @@
 from conectar import connect
 
+def inserir(mydb, email, nome, senha, cpf ):
+        mycursor = mydb.cursor()
+
+        sql = 'INSERT INTO livros (email, nome, senha, CPF ) VALUES (%s, %s, %s, %s)'
+        val = (email, nome, senha, cpf)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+
+        print(mycursor.rowcount, "o usuario foi adicionado no nosso sistema, obrigado pela ajuda!")
+
+        mycursor.close()
+
